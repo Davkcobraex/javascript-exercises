ba76c1d3f5aac3f785cafd584cdcd176546e4642
Davkcobraex <120555663+Davkcobraex@users.noreply.github.com>
2023-12-13 19:46:00 +1000

    complete js excerises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const initalString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return initalString.split("").reverse().join("") == initalString;
 };
-
+palindromes("string banana");
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let lastNum = 1;
+    let currentNum = 0;
 
+    if (num < 0)  {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < num; i++) {
+        const sum = lastNum + currentNum;
+        lastNum = currentNum;
+        currentNum = sum;
+
+    }
+    return currentNum;
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
